c1a178ec112cb6612b3418489d71501d6c2c6c2d
jp <jay.jp209@gmail.com>
2024-03-15 01:32:57 -0400

    fix jenkins file

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const server = app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
-module.exports = app;
+module.exports = server;
--- test.js
+++ test.js
@@ -1,9 +1,18 @@
-const request = require("supertest");
-const app = require("./index");
+const request = require('supertest');
+const server = require('./index');
 
-describe("Test the root path", () => {
-  test("It should response the GET methods", async () => {
-    const response = await request(app).get("/");
+describe('Test the root path', () => {
+  test('It should respond to GET method', async () => {
+    const response = await request(server).get('/');
     expect(response.statusCode).toBe(200);
   });
+});
+
+afterAll(done => {
+  if (server && typeof server.close === 'function') {
+    server.close(done);
+  } else {
+    console.warn('Unable to close server: No close() method found.');
+    done();
+  }
 });
